6e03302ae1a4981d16b27c7cb4794b8bcff5e9b9
Shriram Bhardwaj <shrirambhardwaj68@gmail.com>
2023-08-15 05:33:53 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }
   return (
     <>
-      <Router>
+      <Router basename='/Word_counter'>
         <CssBaselines />
         <ResponsiveAppBar checked={checked} change={handleswitch} />
         <Alert_system msg={msg} />
